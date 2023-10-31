d86d55725f6cc5bca8fd5675b6ccdcacf6d82def
Mikail <miklsuhanov@gmail.com>
2023-10-31 13:09:23 +1000

    uppdate database.sql & create Parsing

--- bin/parsingPurchases.php
+++ bin/parsingPurchases.php
@@ -1,7 +1,5 @@
 <?php
 
-use GuzzleHttp\Client;
-use DiDom\Document;
 use Mikail325\TestTask100sp\Connection;
 use Mikail325\TestTask100sp\Migration;
 
@@ -16,24 +14,3 @@ if (file_exists($autoloadPath1)) {
 
 $pdo = Connection::connect();
 Migration::migrate($pdo);
-
-$url = 'https://www.100sp.ru/vladivostok';
-$client = new Client();
-$response = $client->request('GET', $url);
-$html = $response->getBody()->getContents();
-$document = new Document();
-$document->loadHtml($html);
-$typePurchases = $document->find('div.purchases h2 a');
-$purchases = $document->find('div.span12 div div div.purchase-block div.picture');
-$purchasesName = $document->find('div.span12 div div div.purchase-block div.properties div.name a');
-// var_dump($purchases->html());
-foreach ($purchases as $key => $element) {
-    print_r($element->closest('.purchases')->first('h2')->text());
-    print_r(' ');
-    print_r($element->first('a')->href);
-    print_r(' ');
-    print_r($element->first('img')->attr('data-src'));
-    print_r(' ');
-    print_r($purchasesName[$key]->text());
-    print_r("\n");
-}
