d73c2665f80196681125eeab81781a063fe9c2ba
Mikail <miklsuhanov@gmail.com>
2023-11-01 12:52:14 +1000

    create tabel sities in database.sql

--- config/database.sql
+++ config/database.sql
@@ -1,11 +1,16 @@
-DROP TABLE IF EXISTS purchases, type_purchases;
-CREATE TABLE IF NOT EXISTS type_purchases (
+DROP TABLE IF EXISTS purchases, type_purchases, sities;
+CREATE TABLE type_purchases (
             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             name varchar(255)
             );
-CREATE TABLE IF NOT EXISTS purchases (
+CREATE TABLE sities (
+            id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name varchar(255)
+            );
+CREATE TABLE purchases (
             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             type_id bigint REFERENCES type_purchases (id),
+            siti_id bigint REFERENCES sities (id),
             name varchar(255),
             url_foto varchar(255),
             url varchar(255)
