df90d36742de9b4afa6344b31d94f837e89638d6
Mikail <miklsuhanov@gmail.com>
2023-11-01 16:46:48 +1000

    adding creating a city table in database.sql

--- config/database.sql
+++ config/database.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS purchases, type_purchases, sities;
+DROP TABLE IF EXISTS purchases, type_purchases, cities;
 CREATE TABLE type_purchases (
             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             name varchar(255)
             );
-CREATE TABLE sities (
+CREATE TABLE cities (
             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
             name varchar(255)
             );
 CREATE TABLE purchases (
             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            citi_id bigint REFERENCES cities (id),
             type_id bigint REFERENCES type_purchases (id),
-            siti_id bigint REFERENCES sities (id),
             name varchar(255),
             url_foto varchar(255),
             url varchar(255)
